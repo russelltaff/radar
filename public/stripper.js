fb31caadb07b197720c6a5440c07524a16f99508
daviskoh <koh.davis.0@gmail.com>
2013-11-06 10:45:34 -0500

    specify bill value

--- public/stripper.js
+++ public/stripper.js
@@ -1,8 +1,14 @@
 window.onload = function() {
   var stripper = document.querySelector("img#stripper");
+  var bill = document.querySelector('input#cash-selecta');
 
   stripper.addEventListener('mouseover', function() {
-    makeRain(this);
+    if (bill.value === '1' || bill.value === '5' || bill.value === '10' || bill.value === '20' || bill.value === '50' || bill.value === '100') {
+      makeRain(this);
+    } else {
+      alert("Really...a $" + bill.value + "bill?")
+    }
+
   });
 
   // setInterval( moveDollas, 500);
@@ -10,7 +16,9 @@ window.onload = function() {
 
 function makeRain() {
   var div = document.createElement("div");
-  div.innerHTML = "$1";
+  var bill = document.querySelector('input#cash-selecta');
+
+  div.innerHTML = "$" + bill.value;
   div.className = "dollas";
   document.body.appendChild(div);
   
@@ -27,7 +35,8 @@ function tunes() {
 function renderRacks() {
   var racksSpent = document.querySelector('span');
   var number = parseInt(racksSpent.innerHTML);
-  racksSpent.innerHTML = number += 1;
+  var bill = document.querySelector('input#cash-selecta');
+  racksSpent.innerHTML = number += parseInt(bill.value);
   racksSpent.color = "green";
 }
 
