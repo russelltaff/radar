dbe4d26423efdd0dce7f480f244257a008a51dd8
millzpaugh <ann.millspaugh@gmail.com>
2013-11-11 15:12:40 -0500

    user can create account

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131111200355) do
+ActiveRecord::Schema.define(version: 20131111201004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20131111200355) do
   create_table "users", force: true do |t|
     t.string   "name",                null: false
     t.string   "email",               null: false
-    t.string   "password",            null: false
+    t.string   "password_digest",     null: false
     t.string   "type",                null: false
     t.string   "city"
     t.string   "state"
