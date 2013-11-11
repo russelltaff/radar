02904891143efa2c86d84cbf4ae363da902b818b
Russell Taff <rtaff2012@gmail.com>
2013-11-11 15:50:33 -0500

    added company size migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Radar::Application.routes.draw do
-<<<<<<< HEAD
+
   root to: 'sessions#new'
 
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy] 
-=======
-
->>>>>>> master
 end
