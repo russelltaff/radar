8a1dee9bf1115abd3ba40e63c51d8c9683aff3b2
Russell Taff <rtaff2012@gmail.com>
2013-11-12 18:40:29 -0500

    join table postskills

---
+++ db/migrate/20131112233527_posts_skills.rb
@@ -0,0 +1,21 @@
+class PostsSkills < ActiveRecord::Migration
+  def change
+    create table :posts_skills do |t|
+      t.belongs_to :post, null: false
+      t.belongs_to :skill, null: false
+    end
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL
+          ALTER TABLE posts_skills
+          ADD CONSTRAINT fk_posts
+          FOREIGN KEY (post_id)
+          REFERENCES posts(id),
+          ADD CONSTRAINT fk_skills
+          FOREIGN KEY (skill_id)
+          REFERENCES skills(id)
+        SQL
+      end
+    end
+  end
+end
