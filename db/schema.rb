a9352a09b09e89365355a44bb44297dc0434a509
millzpaugh <ann.millspaugh@gmail.com>
2013-11-12 18:25:12 -0500

    added posts and skills tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131111204837) do
+ActiveRecord::Schema.define(version: 20131112231043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: true do |t|
+    t.integer  "user_id",       null: false
+    t.boolean  "for_hire"
+    t.text     "description",   null: false
+    t.string   "position_type", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
+
+  create_table "skills", force: true do |t|
+    t.string "name", null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "name",                null: false
