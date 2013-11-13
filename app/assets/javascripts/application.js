3e10665325d5e4560b23a2f716fa3a9228b87f32
Raleigh D Froeber <RDFroeber@gmail.com>
2013-11-13 14:37:53 -0500

    Navigation fully migrated to css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,26 +15,3 @@
 //= require turbolinks
 //= require_tree .
 
-
-var addHoverListener = function(){
-   $($("ul.dropdown_profile").parent()).hover(function(){
-      $(this.children[1]).css("display", "block");
-   }, function() {
-      $(this.children[1]).css("display", "none");    
-   });
-   $($("ul.dropdown_post").parent()).hover(function(){
-      $(this.children[1]).css("display", "block");
-   }, function() {
-      $(this.children[1]).css("display", "none");    
-   });
-    $($("ul.dropdown_search").parent()).hover(function(){
-      $(this.children[1]).css("display", "block");
-   }, function() {
-      $(this.children[1]).css("display", "none");    
-   });
-}
-
-
-
-$(document).ready(addHoverListener); //adds when *user* loads or refreshes
-$(document).on("page:change", addHoverListener); //adds when rails loads
