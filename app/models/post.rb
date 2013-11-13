b101541dfb6a5259942a2757b8c58547a376abc1
daviskoh <koh.davis.0@gmail.com>
2013-11-13 12:32:10 -0500

    drop posts_skills, skills & comment out scope in posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ActiveRecord::Base
   #has_and_belongs_to_many :skills 
   acts_as_taggable 
   acts_as_taggable_on :skill_tags 
-  scope :by_join_date, order("created_at DESC")
+  # scope :by_join_date, order("created_at DESC")
 end
