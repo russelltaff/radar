359b942c88896c07400387f002ad2143c0a76c36
daviskoh <koh.davis.0@gmail.com>
2013-11-13 23:13:51 -0500

    setup tags.js

---
+++ app/assets/javascripts/tags.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require jquery_ujs
+
+// enter tag into field
+// press enter/click
+// attach tag to post
+  // add to array
+  set skills_tags
+// render tag inside post div
+// clear tag text field
