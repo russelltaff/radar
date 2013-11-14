1b91be3c00abbab1b11e95ecbaf0fe16c85706e9
Raleigh D Froeber <RDFroeber@gmail.com>
2013-11-14 17:04:58 -0500

    Images added, posts search isotoped, posts routes revised, welcome page added

---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,35 @@
+module PostsHelper
+
+   def type_of_position(post_obj)
+      case post_obj.position_type
+      when "Product Manager"
+         @job_class = "product_man"
+      when "Entrepreneur"
+         @job_class = "entrep"
+      when "Digital Marketer"
+         @job_class = "digital"
+      when "Founder/Co-Founder"
+         @job_class = "founder"
+      when "UX Designer"
+         @job_class = "ux"
+      when "Product Designer"
+         @job_class = "product_des"
+      when "Full-Stack Developer"
+         @job_class = "full_dev"
+      when "Back-end Developer"
+         @job_class = "back_dev"
+      when "Front-end Developer"
+         @job_class = "front_dev"
+      else
+         @job_class = "" 
+      end
+   end
+
+   def type_of_post(post_obj)
+      if post_obj.for_hire == true
+         @type_class = "for_hire"
+      else
+         @type_class = "hiring"
+      end
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,23 @@
 Radar::Application.routes.draw do
 
-  root to: 'sessions#new'
+  root to: 'users#welcome'
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create] do
+    collection do
+      get 'welcome'
+    end
+  end
 
   resources :students, only: [:show, :edit, :update] do 
-    collection do
-      get "search"
-    end
-    
     resources :posts, except: [:index, :edit, :update]
   end 
 
   resources :employers, only: [:show, :edit, :update] do 
-    collection do
-      get "search"
-    end
-
     resources :posts, except: [:index, :edit, :update]
   end 
 
   resources :posts, only: [:show, :index]
   
   resource :session, only: [:new, :create, :destroy] 
-
 
 end
